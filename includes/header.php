d7db4e00b3fa50c53ef8252561b75b3a47aee203
coderEdu <educoder78@gmail.com>
2022-08-25 21:10:24 -0300

    include tailwind cdn and add some changes

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,8 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
+    <script src="https://cdn.tailwindcss.com"></script>
+
     <!-- My own CSS file-->
     <!--<link rel="stylesheet" href="css/style.css">-->
 
@@ -17,38 +19,55 @@
 
     <title>PhoneBook Management</title>
   </head>
-  <body style="background-color: #f0f4f7;">
-    <div class="container-fluid">
+  <!-- <body style="background-color: #f0f4f7;"> -->
+  <body class="bg-slate-100">
+
+    <div class="container w-full">
       <?php  ?>
-      <div class="row text-light p-3 align-items-center" style="border-bottom: 9px solid #6495ED; background-color: #607bab;">
-        <div class="col-sm-1" style="width: 80px;">
-          <?php
-          include_once('functions.php');
+      <div class="bg-teal-300 mt-6 border-2 border-teal-400 rounded-2xl shadow-lg">        
+        <div class="grid grid-cols-3 p-3"> <!-- begin grid -->
+          <div class="grid grid-cols-3 place-content-center"> <!-- begin first column -->
+            <div class="">
+              <?php
+              include_once('functions.php');
 
-          if (isset($_SESSION['go_home'])) {
-            $url=get_home_url();
-          } else { 
-            $url="";
-          }
-          ?>
-          <a href="<?php echo $url ?>"><img src="img/iPhone-icon.png" alt="" width="84"></a>
-        </div>
-        <div class="col-sm-3">
-          <h2 style="display: inline; font-family: Verdana;"><?php echo strtoupper("PhoneBook Manager")?></h2>
-        </div>
-        <div class="col-sm-3">
-          <h3 class="align-middle text-end mb-0"><?php if (isset($_SESSION['user-name'])) {echo 'Hi '.$_SESSION['user-name'].'!';} ?></h3>
-        </div>
-        <div class="col-sm-1">
+              if (isset($_SESSION['go_home'])) {
+                $url=get_home_url();
+              } else { 
+                $url="";
+              }
+              ?>
+              <div class="w-32 mx-auto">
+                <a href="<?php echo $url ?>"><img src="img/iPhone-icon.png" alt=""></a>
+              </div>
+            </div>
+            <div class="grid col-span-2 w-full h-full place-items-center">
+              <!-- <h2 style="display: inline; font-family: Verdana;"><?php echo strtoupper("PhoneBook Manager")?></h2> -->
+              <span class="inline-block font-serif text-3xl"><?php echo strtoupper("PhoneBook Manager")?></span>
+            </div>
+          </div> <!-- finish first column -->
 
-        <?php if (isset($_SESSION['user-name'])) { ?>          
-          <form action="logout.php" method="POST">
-            <input type="submit" class="btn btn-danger" name="logout" value="Log out">
-          </form>
-        <?php } ?>      
-          
+          <div class=""> <!-- begin second column -->
+            <div class="grid grid-cols-3 w-full h-full place-items-center">
+              <div class="col-span-2 mx-auto">
+                <span class="font-sans text-3xl"><?php if (isset($_SESSION['user-name'])) {echo 'Hi '.$_SESSION['user-name'].'!';} ?></span>
+              </div>
+
+              <div class="">
+              <?php if (isset($_SESSION['user-name'])) { ?>          
+                <form action="logout.php" method="POST">
+                  <input type="submit" class="bg-red-600 text-white py-2 px-3 rounded-lg" name="logout" value="Log out">
+                </form>
+              <?php } ?>              
+              </div>
+            </div>
+          </div> <!-- finish second column -->
+
+          <div> <!-- begin third column -->
+            <div class="grid place-items-center w-full h-full">
+            <?php if (isset($_SESSION['user-name'])) {include "includes/search-bar.php";} ?>
+            </div>
+          </div> <!-- finish third column -->
         </div>
-        <div class="col-sm-1"></div>
-        <?php if (isset($_SESSION['user-name'])) {include "includes/search-bar.php";} ?>
       </div>
     </div>
--- includes/search-bar.php
+++ includes/search-bar.php
@@ -1,5 +1,3 @@
-<div class="col-sm-3 ps-5 ms-5">
-  <form action="home.php" method="GET">
-    <input type="text" class="form-control" name="search" placeholder="Type here to search">
-  </form>
-</div>
+<form action="home.php" method="GET">
+  <input type="text" class="right-0 border-neutral-200 focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 bg-white dark:border-neutral-700 dark:focus:ring-primary-500 dark:focus:ring-opacity-25 dark:bg-neutral-900 rounded-2xl text-sm font-normal h-11 px-4 py-3 mt-1" name="search" placeholder="Type here to search">
+</form>
